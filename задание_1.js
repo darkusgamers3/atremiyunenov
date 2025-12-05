b5b13649d5e21f1a379ff8c382d61621859e5519
Артемий <steamfg9@gmail.com>
2025-12-05 10:34:57 +0300

    Add files via upload

---
+++ задание_1.js
@@ -0,0 +1,7 @@
+function containsChar(str, char) {
+ return str.includes(char);
+}
+// Примеры вызова:
+console.log(containsChar('hello', 'e')); // true
+console.log(containsChar('world', 'z')); // false
+console.log(containsChar('Hello', 'h')); // false
